2eca5116f5f1009332b5e369a1d643f3959c171d
David Vavrecka <david.vavrecka@gmail.com>
2023-10-20 23:27:36 +0200

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Ahoj, jmenuji se David a tohle je můj web.</p>
+        <p>Ahoj, jmenuji se David a tohle je můj web!</p>
       </header>
     </div>
   );
